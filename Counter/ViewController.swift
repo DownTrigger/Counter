de55204c44c712976904c89cdd33d334cd7c4ab0
andrew.ruzavin <andrew.ruzavin@gmail.com>
2025-08-26 00:13:54 +0700

    added implementation of the basic task:  UILabel and UIButton

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var incrementButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        incrementButton.setTitle("+", for: .normal)
+        counterLabel.text = "Значение счетчика: 0"
     }
-
-
+    
+    var counter: Int = 0
+    
+    @IBAction func didTapIncrementButton(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "Значение счетчика: \(counter)"
+    }
+    
 }
 
