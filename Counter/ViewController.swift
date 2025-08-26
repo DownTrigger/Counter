0c8f76a41e498c727f2c8b7821ec62df214ba951
andrew.ruzavin <andrew.ruzavin@gmail.com>
2025-08-26 10:15:45 +0700

    Added a UITextView field + a minus button and a reset button

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,23 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var historyTextView: UITextView!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var decrementButton: UIButton!
     @IBOutlet weak var incrementButton: UIButton!
     @IBOutlet weak var counterLabel: UILabel!
+    
+    private var counter: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        incrementButton.setTitle("+", for: .normal)
-        counterLabel.text = "Значение счетчика: 0"
+        counterLabel.text = "0"
+        
+        incrementButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        incrementButton.setTitle("", for: .normal)
+        incrementButton.backgroundColor = .red
+        incrementButton.layer.cornerRadius = 5
+        
+        decrementButton.setImage(UIImage(systemName: "minus"), for: .normal)
+        decrementButton.setTitle("", for: .normal)
+        decrementButton.backgroundColor = .blue
+        decrementButton.layer.cornerRadius = 5
+        
+        resetButton.setImage(UIImage(systemName: "arrow.counterclockwise"), for: .normal)
+        resetButton.setTitle("", for: .normal)
+        resetButton.backgroundColor = .green
+        resetButton.layer.cornerRadius = 5
     }
-    
-    var counter: Int = 0
     
     @IBAction func didTapIncrementButton(_ sender: Any) {
         counter += 1
-        counterLabel.text = "Значение счетчика: \(counter)"
+        counterLabel.text = "\(counter)"
     }
     
+    @IBAction func didTapDecrementButton(_ sender: Any) {
+        counter -= 1
+        counterLabel.text = "\(counter)"
+    }
+  
+    @IBAction func didTapResetButton(_ sender: Any) {
+        counter = 0
+        counterLabel.text = "\(counter)"
+    }
 }
 
