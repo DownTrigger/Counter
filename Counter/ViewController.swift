f45408640363dde42e6eb8bb4f60eefbf96bef6d
andrew.ruzavin <andrew.ruzavin@gmail.com>
2025-08-26 12:22:10 +0700

    Configure layout constraints for all UI elements in storyboard

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
     
     private let formatter: DateFormatter = {
         let dateString = DateFormatter()
-        dateString.dateFormat = "dd.MM HH:mm:ss"
+        dateString.dateFormat = "[dd.MM.yyyy HH:mm:ss]"
         return dateString
     }()
     
@@ -42,17 +42,20 @@ class ViewController: UIViewController {
         
         incrementButton.setImage(UIImage(systemName: "plus"), for: .normal)
         incrementButton.setTitle("", for: .normal)
-        incrementButton.backgroundColor = .red
+        incrementButton.backgroundColor = .systemRed
+        incrementButton.tintColor = .white
         incrementButton.layer.cornerRadius = 5
         
         decrementButton.setImage(UIImage(systemName: "minus"), for: .normal)
         decrementButton.setTitle("", for: .normal)
-        decrementButton.backgroundColor = .blue
+        decrementButton.backgroundColor = .systemBlue
+        decrementButton.tintColor = .white
         decrementButton.layer.cornerRadius = 5
         
         resetButton.setImage(UIImage(systemName: "arrow.counterclockwise"), for: .normal)
         resetButton.setTitle("", for: .normal)
-        resetButton.backgroundColor = .green
+        resetButton.backgroundColor = .systemGreen
+        resetButton.tintColor = .white
         resetButton.layer.cornerRadius = 5
         
         updateUI()
@@ -62,29 +65,26 @@ class ViewController: UIViewController {
     @IBAction func didTapIncrementButton(_ sender: Any) {
         counter += 1
         let dateString = formatter.string(from: Date())
-        historyTextView.text = "\(history)" + "\n\(dateString) : Значение изменено на +1"
+        historyTextView.text = "\(history)" + "\n\(dateString): Значение изменено на +1"
         history = historyTextView.text
-        autoScroll()
     }
     
     @IBAction func didTapDecrementButton(_ sender: Any) {
         let dateString = formatter.string(from: Date())
         if counter > 0 {
             counter -= 1
-            historyTextView.text = "\(history)" + "\n\(dateString) : Значение изменено на -1"
+            historyTextView.text = "\(history)" + "\n\(dateString): Значение изменено на -1"
         } else {
-            historyTextView.text = "\(history)" + "\n\(dateString) : Попытка уменьшить значение счётчика ниже 0"
+            historyTextView.text = "\(history)" + "\n\(dateString): Попытка уменьшить значение счётчика ниже 0"
         }
         history = historyTextView.text
-        autoScroll()
     }
   
     @IBAction func didTapResetButton(_ sender: Any) {
         counter = 0
         let dateString = formatter.string(from: Date())
-        historyTextView.text = "\(history)" + "\n\(dateString) : Значение сброшено"
+        historyTextView.text = "\(history)" + "\n\(dateString): Значение сброшено"
         history = historyTextView.text
-        autoScroll()
     }
     
     private func updateUI() {
@@ -95,10 +95,11 @@ class ViewController: UIViewController {
     
     private func updateHistory() {
         historyTextView.text = "\(history)"
+        autoScroll()
     }
     
     private func autoScroll() {
-        let bottomRange = NSMakeRange(historyTextView.text.count - 1, 1)
+        let bottomRange = NSMakeRange(historyTextView.text.count - 2, 0)
         historyTextView.scrollRangeToVisible(bottomRange)
     }
     
